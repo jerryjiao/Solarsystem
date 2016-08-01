87955c338abc80b05f82d891b0f63fcd8544c52f
jerryjiao <jerry201382@gmail.com>
2016-08-01 19:29:54 +0100

    First upload

---
+++ Solar system vr/Assets/Scripts/Gaze.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gaze : MonoBehaviour {
+
+    private CardboardHead head;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        Cardboard.SDK.OnTrigger += PullTrigger;
+        head = GameObject.FindObjectOfType<CardboardHead>();
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void PullTrigger()
+    {
+        rb.velocity = head.Gaze.direction*10;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+   
+}
